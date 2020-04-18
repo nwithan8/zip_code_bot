5389e0b8fd1dab2fdc07a2100cc74d67519ce84b
Nate Harris <n8gr8gbln@gmail.com>
2020-04-17 20:46:38 -0400

    Initial commit

---
+++ usps/usps.py
@@ -0,0 +1,172 @@
+import json
+import requests
+import xmltodict
+
+from lxml import etree
+
+from .constants import LABEL_ZPL, SERVICE_PRIORITY
+
+
+class USPSApiError(Exception):
+    pass
+
+
+class USPSApi(object):
+    BASE_URL = 'https://secure.shippingapis.com/ShippingAPI.dll?API='
+    urls = {
+        'tracking': 'TrackV2{test}&XML={xml}',
+        'label': 'eVS{test}&XML={xml}',
+        'validate': 'Verify&XML={xml}',
+        'citystate': 'CityStateLookup&XML={xml}',
+        'zipcode': 'ZipCodeLookup&XML={xml}'
+    }
+
+    def __init__(self, api_user_id, test=False):
+        self.api_user_id = api_user_id
+        self.test = test
+
+    def get_url(self, action, xml):
+        return self.BASE_URL + self.urls[action].format(
+            **{'test': 'Certify' if self.test else '', 'xml': xml}
+        )
+
+    def send_request(self, action, xml):
+        # The USPS developer guide says "ISO-8859-1 encoding is the expected character set for the request."
+        # (see https://www.usps.com/business/web-tools-apis/general-api-developer-guide.htm)
+        xml = etree.tostring(xml, encoding='iso-8859-1', pretty_print=self.test).decode()
+        url = self.get_url(action, xml)
+        xml_response = requests.get(url).content
+        response = json.loads(json.dumps(xmltodict.parse(xml_response)))
+        if 'Error' in response:
+            raise USPSApiError(response['Error']['Description'])
+        return response
+
+    def validate_address(self, *args, **kwargs):
+        return AddressValidate(self, *args, **kwargs)
+
+    def track(self, *args, **kwargs):
+        return TrackingInfo(self, *args, **kwargs)
+
+    def create_label(self, *args, **kwargs):
+        return ShippingLabel(self, *args, **kwargs)
+
+    def lookup_city_by_zip(self, *args, **kwargs):
+        return CityStateLookup(self, *args, **kwargs)
+
+    def lookup_zip_by_address(self, *args, **kwargs):
+        return ZipCodeLookup(self, *args, **kwargs)
+
+
+class AddressValidate(object):
+
+    def __init__(self, usps, address, just_answer: bool = False):
+        xml = etree.Element('AddressValidateRequest', {'USERID': usps.api_user_id})
+        _address = etree.SubElement(xml, 'Address', {'ID': '0'})
+        address.add_to_xml(_address, prefix='', validate=True)
+
+        self.result = usps.send_request('validate', xml)
+
+
+class TrackingInfo(object):
+
+    def __init__(self, usps, tracking_number):
+        xml = etree.Element('TrackFieldRequest', {'USERID': usps.api_user_id})
+        child = etree.SubElement(xml, 'TrackID', {'ID': tracking_number})
+
+        self.result = usps.send_request('tracking', xml)
+
+
+class CityStateLookup(object):
+
+    def __init__(self, usps, zip_code, just_answer: bool = False):
+        xml = etree.Element('CityStateLookupRequest', {'USERID': usps.api_user_id})
+        _zip_code = etree.SubElement(xml, 'ZipCode', {'ID': '0'})
+        zip_code_inner = etree.SubElement(_zip_code, 'Zip5')
+        zip_code_inner.text = zip_code
+
+        self.result = usps.send_request('citystate', xml)
+        if just_answer:
+            try:
+                if self.result.get('CityStateLookupResponse').get('ZipCode').get('Error'):
+                    self.result = 'Invalid Zip Code.'
+                else:
+                    self.result = {
+                        'City': self.result.get('CityStateLookupResponse').get('ZipCode').get('City'),
+                        'State': self.result.get('CityStateLookupResponse').get('ZipCode').get('State')
+                    }
+            except:
+                self.result = None
+
+
+class ZipCodeLookup(object):
+
+    def __init__(self, usps, address, just_answer: bool = False):
+        xml = etree.Element('ZipCodeLookupRequest', {'USERID': usps.api_user_id})
+        _address = etree.SubElement(xml, 'Address', {'ID': '1'})
+        address.add_to_xml(_address, prefix='', validate=True)
+
+        self.result = usps.send_request('zipcode', xml)
+        if just_answer:
+            try:
+                if self.result.get('ZipCodeLookupResponse').get('Address').get('Error'):
+                    self.result = 'Invalid Address.'
+                else:
+                    self.result = {
+                        'Zip5': self.result.get('ZipCodeLookupResponse').get('Address').get('Zip5'),
+                        'Zip4': self.result.get('ZipCodeLookupResponse').get('Address').get('Zip4')
+                    }
+            except:
+                self.result = None
+
+
+class ShippingLabel(object):
+
+    def __init__(self, usps, to_address, from_address, weight,
+                 service=SERVICE_PRIORITY, label_type=LABEL_ZPL):
+        root = 'eVSRequest' if not usps.test else 'eVSCertifyRequest'
+        xml = etree.Element(root, {'USERID': usps.api_user_id})
+
+        label_params = etree.SubElement(xml, 'ImageParameters')
+        label = etree.SubElement(label_params, 'ImageParameter')
+        label.text = label_type
+
+        from_address.add_to_xml(xml, prefix='From', validate=False)
+        to_address.add_to_xml(xml, prefix='To', validate=False)
+
+        package_weight = etree.SubElement(xml, 'WeightInOunces')
+        package_weight.text = str(weight)
+
+        delivery_service = etree.SubElement(xml, 'ServiceType')
+        delivery_service.text = service
+
+        etree.SubElement(xml, 'Width')
+        etree.SubElement(xml, 'Length')
+        etree.SubElement(xml, 'Height')
+        etree.SubElement(xml, 'Machinable')
+        etree.SubElement(xml, 'ProcessingCategory')
+        etree.SubElement(xml, 'PriceOptions')
+        etree.SubElement(xml, 'InsuredAmount')
+        etree.SubElement(xml, 'AddressServiceRequested')
+        etree.SubElement(xml, 'ExpressMailOptions')
+        etree.SubElement(xml, 'ShipDate')
+        etree.SubElement(xml, 'CustomerRefNo')
+        etree.SubElement(xml, 'ExtraServices')
+        etree.SubElement(xml, 'HoldForPickup')
+        etree.SubElement(xml, 'OpenDistribute')
+        etree.SubElement(xml, 'PermitNumber')
+        etree.SubElement(xml, 'PermitZIPCode')
+        etree.SubElement(xml, 'PermitHolderName')
+        etree.SubElement(xml, 'CRID')
+        etree.SubElement(xml, 'MID')
+        etree.SubElement(xml, 'LogisticsManagerMID')
+        etree.SubElement(xml, 'VendorCode')
+        etree.SubElement(xml, 'VendorProductVersionNumber')
+        etree.SubElement(xml, 'SenderName')
+        etree.SubElement(xml, 'SenderEMail')
+        etree.SubElement(xml, 'RecipientName')
+        etree.SubElement(xml, 'RecipientEMail')
+        etree.SubElement(xml, 'ReceiptOption')
+        image = etree.SubElement(xml, 'ImageType')
+        image.text = 'PDF'
+
+        self.result = usps.send_request('label', xml)
---
+++ zip_code_bot.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+import praw
+import os, sys
+import re
+import shutil
+import requests
+from usps import USPSApi, Address
+import logging
+import sql_library as sql
+
+USPS_USERID = '256PERSO0573'
+
+# Get at https://www.reddit.com/prefs/apps
+REDDIT_CLIENT_ID = ""
+REDDIT_CLIENT_SECRET = ""
+REDDIT_USERNAME = ""
+REDDIT_PASSWORD = "!"
+
+# Careful as you release this into the wild!
+SUB_TO_MONITOR = "test"
+
+REPLY_TEMPLATE = "ZIP Code {zip} is {city}, {state}"
+REPLY_INVALID_TEMPLATE = '{zip} is not a valid ZIP code, according to the USPS.'
+REPLY_TAG = '\n\n^(Created by u/grtgbln)'
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+
+usps = USPSApi(USPS_USERID)
+
+db = sql.SQL(sql_type='SQLite', sqlite_file='zip_code_entries.db')
+
+reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET,
+                     user_agent='ZipCodeBot (by u/grtgbln)', username=REDDIT_USERNAME, password=REDDIT_PASSWORD)
+if not reddit.read_only:
+    logging.info("Connected and running.")
+
+
+def search_zip(zip_code):
+    zip = usps.lookup_city_by_zip(str(zip_code), just_answer=True)
+    return zip.result
+
+
+def get_zip_code(body):
+    return re.findall('\b\d{5}\b', body)
+
+
+def reply_with_loc(submission, loc, zip_code):
+    try:
+        return submission.reply(REPLY_TEMPLATE.format(zip=zip_code, city=loc['City'], state=loc['State']) + REPLY_TAG)
+    except Exception as e:
+        logging.error(e)
+        return None
+
+
+def reply_invalid(submission, zip_code):
+    try:
+        return submission.reply(REPLY_INVALID_TEMPLATE.format(zip=zip_code) + REPLY_TAG)
+    except Exception as e:
+        logging.error(e)
+        return None
+
+
+def process(entry_type, entry, text):
+    zips = get_zip_code(text)
+    if zips:
+        zip_code = zips[0]
+        if not check_if_already_in_db(entry_type=entry_type, entry_id=entry.id):
+            loc = search_zip(zip_code)
+            if loc and loc != 'Invalid Zip Code.':
+                comment = reply_with_loc(entry, loc, zip_code)
+                if comment:
+                    _ = store_entry_in_db(entry_type, entry.id)
+                    logging.info(f'Replied with location: {comment.permalink}.')
+                else:
+                    logging.error("Couldn't reply to valid location.")
+            else:
+                comment = reply_invalid(entry, zip_code)
+                if comment:
+                    _ = store_entry_in_db(entry_type, entry.id)
+                    logging.info(f'Replied to invalid ZIP: {comment.permalink}.')
+                else:
+                    logging.error("Couldn't reply to invalid location.")
+        else:
+            logging.info(f"Already replied to this {entry_type}")
+    else:
+        logging.debug("No ZIP codes found.")
+
+
+def check_if_already_in_db(entry_type, entry_id):
+    if entry_type == 'submission':
+        results = db.custom_query(queries=[f"SELECT id FROM submissions WHERE id = '{str(entry_id)}'"])
+        if results and results > 0:
+            return True
+        return False
+    elif entry_type == 'comment':
+        results = db.custom_query(queries=[f"SELECT id FROM comments WHERE id = '{str(entry_id)}'"])
+        if results and results > 0:
+            return True
+        return False
+    else:
+        logging.info("entry_type is not 'submission' or 'comment', skipping entry...")
+        return True
+
+
+def store_entry_in_db(entry_type, entry_id):
+    if entry_type == 'submission':
+        results = db.custom_query(queries=[f"INSERT INTO submissions (id) VALUES ('{str(entry_id)}')"], commit=True)
+        if results and results > 0:
+            return True
+        return False
+    elif entry_type == 'comment':
+        results = db.custom_query(queries=[f"INSERT INTO comments (id) VALUES ('{str(entry_id)}')"], commit=True)
+        if results and results > 0:
+            return True
+        return False
+    else:
+        logging.error("Couldn't store entry in database.")
+        return False
+
+
+def clean_db():
+    results = db.custom_query(queries=['DELETE FROM submissions', 'DELETE FROM comments'], commit=True)
+    if results >= 0:
+        logging.info('Database cleared.')
+        return True
+    logging.error('Database could not be cleared.')
+    return False
+
+
+def run():
+    # clean_db()
+    subreddit = reddit.subreddit(SUB_TO_MONITOR)
+    comment_stream = subreddit.stream.comments(pause_after=-1)
+    submission_stream = subreddit.stream.submissions(pause_after=-1)
+    while True:
+        for comment in comment_stream:
+            if comment is None:
+                break
+            if comment.author and comment.author.name and comment.author.name != REDDIT_USERNAME:
+                process(entry_type='comment', entry=comment, text=comment.body)
+        for submission in submission_stream:
+            if submission is None:
+                break
+            if submission.is_self and submission.author and submission.author.name and submission.author.name != REDDIT_USERNAME:
+                process(entry_type='submission', entry=submission, text=submission.selftext)
+
+
+run()
